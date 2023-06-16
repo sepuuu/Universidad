2537fb0d731230eee23d2c4aec495bb25c0af53b
Matías Sepúlveda <71105266+sepuuu@users.noreply.github.com>
2023-06-16 01:09:57 -0400

    cosas

--- Algoritmos/algoritmoGenetico/prueba2.py
+++ Algoritmos/algoritmoGenetico/prueba2.py
@@ -1,78 +1,110 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
 
-N = 25 # tamaño del tablero
-M = 50 # número de genes
-P = 0.05 # probabilidad de mutación
+N = 50  # tamaño del tablero
+M = 100  # número de genes
+##el numero de genes tiene que ser el doble de la dimension del tablero (hay que modificarlo)
 
 # Matriz del tablero
 matriz = np.zeros((N, N))
 
-# Inicializa genes con distribución uniforme de movimientos
+# Inicializa genes en posiciones aleatorias sin duplicados
 genes = []
-for _ in range(M):
-    gen = list(range(8)) * (20 // 8)  # repite la lista de movimientos hasta alcanzar 20 elementos
-    random.shuffle(gen)  # desordena aleatoriamente los movimientos
-    genes.append((gen, (0, random.choice(range(N)))))
+posiciones_ocupadas = set()
+while len(genes) < M:
+    gen = [random.uniform(0, 1) for _ in range(8)]
+    suma = sum(gen)
+    gen = [g / suma for g in gen]
+    pos = (0, random.choice(range(N)))
+    if pos not in posiciones_ocupadas:
+        genes.append((gen, pos))
+        posiciones_ocupadas.add(pos)
+
+# Matriz de movimientos
+movs = [(0, 1), (0, -1), (-1, 0), (1, 0), (-1, 1), (1, 1), (-1, -1), (1, -1)]
 
 # Función para mover un gen de acuerdo a su ADN
 def mover(gen, pos):
-    movs = [(0, 1), (0, -1), (-1, 0), (1, 0), (-1, 1), (1, 1), (-1, -1), (1, -1)]
     x, y = pos
-    for i in range(20):  # limite de pasos
-        g = gen[i%8]    # repite los pasos en el gen después de 8
-        dx, dy = movs[g]
+    for g in range(17):  # limite de pasos reducido
+        idx = random.choices(range(8), weights=gen)[0]
+        dx, dy = movs[idx]
         nx, ny = x + dx, y + dy
-        if nx < 0 or nx >= N or ny < 0 or ny >= N: continue
+        if nx < 0 or nx >= N or ny < 0 or ny >= N:
+            continue
         x, y = nx, ny
-    return (x, y), i + 1  # devuelve la posición final y el número de pasos
+        if x == N-1:  # zona objetivo ahora es la última columna
+            break
+    return (x, y)
+
+# Función para evaluar la calidad de los genes
+def evaluar_calidad(genes):
+    distancias = []
+    for gen, pos in genes:
+        x, y = pos
+        if x == N-1:  # zona objetivo ahora es la última columna
+            distancia = 0
+        else:
+            distancia = N-1 - x  # distancia a la última columna
+        distancias.append(distancia)
+    return distancias
 
 # Cruce de genes con selección de los mejores
-def cruce(genes):
-    genes.sort(key=lambda x: x[1][0] + 1 / x[2], reverse=True)  # ordena por fitness
-    sobrevivientes = genes[:M//2]  # toma la mitad superior
+def cruce(genes, distancias):
+    orden = np.argsort(distancias)
+    mejores_genes = [genes[i] for i in orden[:M//2]]
     nueva_gen = []
-    fitness_total = sum(x[1][0] + 1 / x[2] for x in sobrevivientes)
     for _ in range(M):
-        r = random.uniform(0, fitness_total)
-        acum = 0
-        for gen in sobrevivientes:
-            if acum + gen[1][0] + 1 / gen[2] > r:
-                padre1 = gen
-                break
-            acum += gen[1][0] + 1 / gen[2]
-        r = random.uniform(0, fitness_total)
-        acum = 0
-        for gen in sobrevivientes:
-            if acum + gen[1][0] + 1 / gen[2] > r:
-                padre2 = gen
-                break
-            acum += gen[1][0] + 1 / gen[2]
+        gen1, gen2 = random.choices(mejores_genes, k=2)
         punto = random.choice(range(8))
-        hijo = padre1[0][:punto] + padre2[0][punto:]
-        # Añade mutación
-        if random.random() < P:
-            hijo[random.choice(range(8))] = random.choice(range(8))
-        nueva_gen.append((hijo, (0, random.choice(range(N)))))
+        hijo = gen1[0][:punto] + gen2[0][punto:]
+        suma = sum(hijo)
+        hijo = [g / suma for g in hijo]
+        pos_hijo = gen1[1]
+        nueva_gen.append((hijo, pos_hijo))
     return nueva_gen
 
 # Visualización de los datos con matplotlib
-def graficar(genes):
-    tablero = np.zeros((N, N))
-    for gen, pos, _ in genes:
-        tablero[pos] = 1
-    plt.imshow(tablero, cmap='gray')
-    plt.show()
+def graficar(genes, goal, generation):
+    plt.clf()
+    cmap = ScalarMappable(cmap='hsv').cmap
+    colors = cmap(np.linspace(0, 1, generation + 1))
+    for i, person in enumerate(genes):
+        plt.plot(person[1][0], person[1][1], 'o', color=colors[i % generation])
+    plt.plot([N-1, N-1], [0, N-1], 'b', linewidth=2)  # zona objetivo ahora es la última columna
+    plt.xlim(0, N-1)
+    plt.ylim(0, N-1)
+    plt.title(f'Generation: {generation}')
+    plt.pause(0.5)
 
 # Ciclo principal
-mejor_gen = None
-for generacion in range(20):
-    print(f"Generación {generacion}")
-    genes = [(gen, *mover(gen, pos)) for gen, pos in genes]
-    graficar(genes)
-    genes = cruce(genes)
-    if mejor_gen is None or genes[0][1][0] > mejor_gen[1][0]:
-        mejor_gen = genes[0]
+best_distance = float('inf')
+best_gen = None
+best_generation = 0
 
-print("Mejor gen:", mejor_gen)
+for generation in range(1, 21):
+    print(f"Generación {generation}")
+    genes = [(gen, mover(gen, pos)) for gen, pos in genes]
+    distancias = evaluar_calidad(genes)
+    graficar(genes, N-1, generation)
+    genes = cruce(genes, distancias)
+
+    distancias_finales = evaluar_calidad(genes)
+    mejor_gen_idx = np.argmin(distancias_finales)
+    mejor_gen = genes[mejor_gen_idx][0]
+    mejor_distancia = distancias_finales[mejor_gen_idx]
+    
+    if mejor_distancia < best_distance:
+        best_distance = mejor_distancia
+        best_gen = mejor_gen
+        best_generation = generation
+
+print(f"El mejor gen encontrado es: {best_gen}")
+print(f"Generación en la que se encontró el mejor gen: {best_generation}")
+plt.legend(['Generation 1', 'Generation 2', 'Generation 3', 'Generation 4', 'Generation 5',
+            'Generation 6', 'Generation 7', 'Generation 8', 'Generation 9', 'Generation 10',
+            'Generation 11', 'Generation 12', 'Generation 13', 'Generation 14', 'Generation 15',
+            'Generation 16', 'Generation 17', 'Generation 18', 'Generation 19', 'Generation 20'])
+plt.show()
---
+++ Programacion-3/arboles/apuntes.md
@@ -0,0 +1,57 @@
+# Árboles Binarios de Búsqueda
+
+## Definición de árbol binario de búsqueda
+Un árbol binario de búsqueda es una estructura de datos jerárquica donde cada nodo tiene un valor asociado y cumple con la propiedad de que el valor de cada nodo en el subárbol izquierdo es menor que el valor del nodo y el valor de cada nodo en el subárbol derecho es mayor que el valor del nodo.
+
+## Estructura de un nodo del árbol
+Cada nodo del árbol binario de búsqueda tiene tres componentes principales:
+- Valor: El valor almacenado en el nodo.
+- Subárbol izquierdo: Una referencia al subárbol izquierdo, que contiene valores menores que el valor del nodo actual.
+- Subárbol derecho: Una referencia al subárbol derecho, que contiene valores mayores que el valor del nodo actual.
+
+## Inserción en un árbol binario de búsqueda
+Para insertar un nuevo nodo en un árbol binario de búsqueda, se sigue el siguiente pseudocódigo:
+
+# Árboles Binarios de Búsqueda
+
+## Definición de árbol binario de búsqueda
+Un árbol binario de búsqueda es una estructura de datos jerárquica donde cada nodo tiene un valor asociado y cumple con la propiedad de que el valor de cada nodo en el subárbol izquierdo es menor que el valor del nodo y el valor de cada nodo en el subárbol derecho es mayor que el valor del nodo.
+
+## Estructura de un nodo del árbol
+Cada nodo del árbol binario de búsqueda tiene tres componentes principales:
+- Valor: El valor almacenado en el nodo.
+- Subárbol izquierdo: Una referencia al subárbol izquierdo, que contiene valores menores que el valor del nodo actual.
+- Subárbol derecho: Una referencia al subárbol derecho, que contiene valores mayores que el valor del nodo actual.
+
+## Inserción en un árbol binario de búsqueda
+Para insertar un nuevo nodo en un árbol binario de búsqueda, se sigue el siguiente pseudocódigo:
+
+insertar(nodo, valor):
+si nodo es nulo:
+crear un nuevo nodo con el valor y devolverlo
+si valor es menor que nodo.valor:
+nodo.izquierdo = insertar(nodo.izquierdo, valor)
+si valor es mayor que nodo.valor:
+nodo.derecho = insertar(nodo.derecho, valor)
+devolver nodo
+
+
+
+## Búsqueda en un árbol binario de búsqueda
+Para buscar un valor en un árbol binario de búsqueda, se sigue el siguiente pseudocódigo:
+
+buscar(nodo, valor):
+si nodo es nulo o nodo.valor es igual a valor:
+devolver nodo
+si valor es menor que nodo.valor:
+devolver buscar(nodo.izquierdo, valor)
+si valor es mayor que nodo.valor:
+devolver buscar(nodo.derecho, valor)
+
+
+## Recorrido de un árbol binario de búsqueda
+Hay tres tipos principales de recorrido en un árbol binario de búsqueda:
+- Recorrido en orden (in-order): Se visita primero el subárbol izquierdo, luego el nodo actual y finalmente el subárbol derecho.
+- Recorrido en preorden (pre-order): Se visita primero el nodo actual, luego el subárbol izquierdo y finalmente el subárbol derecho.
+- Recorrido en postorden (post-order): Se visita primero el subárbol izquierdo, luego el subárbol derecho y finalmente el nodo actual.
+
